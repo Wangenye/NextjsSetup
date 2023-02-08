38c0375b37cf1d30ee1001b66c99602a5ad809c4
Simon_mwas <simon.mwangi@touchinspiration.com>
2023-02-08 13:20:46 +0300

    feat: storybook and mocks

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  reactStrictMode: true,
+  images: {
+    domains: ['i.pravatar.cc', 'cdn.pixabay.com'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
